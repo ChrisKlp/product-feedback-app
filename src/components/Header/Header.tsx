c69491372482a6eb23cc9ae1da6dad630d1f84f1
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2024-04-13 11:58:09 +0200

    Update colors and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,26 @@ import Image from 'next/image'
 import backgroundHeader from '@/assets/suggestions/mobile/background-header.png'
 import HamburgerMenu from '@/assets/shared/mobile/icon-hamburger.svg'
 import IconClose from '@/assets/shared/mobile/icon-close.svg'
+import styles from './Header.module.css'
 
 export default function Header() {
   return (
     <header className="w-full">
-      <div className="relative h-[72px]">
+      <div className={styles.logoContainer}>
         <Image
           alt="background header"
           src={backgroundHeader}
           fill
           className="object-cover"
         />
-        <div className="relative flex h-full w-full items-center justify-between px-[24px] text-white">
+        <div className={styles.logoWrapper}>
           <div>
-            <p className="text-[15px] font-bold">Frontend Mentor</p>
-            <p className="text-[13px] font-medium opacity-75">Feedback Board</p>
+            <p className={styles.logoTitle}>Frontend Mentor</p>
+            <p className={styles.logoSubtitle}>Feedback Board</p>
           </div>
-          <HamburgerMenu className="flex" />
+          <button className="-mr-1 p-1">
+            <HamburgerMenu className="flex" />
+          </button>
         </div>
       </div>
       <div className="absolute hidden">
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type Feedback = {
+  id: number
+  title: string
+  category: string
+  upvotes: number
+  status: string
+  description: string
+}
